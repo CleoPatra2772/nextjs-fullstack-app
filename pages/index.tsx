84886a9215755d84197a30a38632e0a9afb4820b
Cleo Gao <celeo127@outlook.com>
2022-10-23 17:17:35 +0800

    finish styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,30 @@
-
+import Image from "next/image";
 import PrimaryLayout from "../components/layouts/primary/PrimaryLayout";
 import { NextPageWithLayout } from './page';
 import Search from "../components/utility/search/Search";
+import logo from '../assets/img/google-logo.png';
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Home: NextPageWithLayout = () => {
+  const { locale } = useRouter();
+
   return (
-    <section>
+    <section className="flex flex-col items-center gap-y-5 mt-12 sm:mt-36">
+      <Image
+      src={logo}
+      alt='Google Logo'
+      width={272}
+      height={92}
+      priority
+      />
         <Search />
+        <p>
+          Google offered in: {' '}
+          <Link href="/" locale={locale == 'en' ? 'fr' : 'en'}>
+            <a className="underline text-blue-600">Francais</a>
+          </Link>
+        </p> 
     </section>
   );
 };
