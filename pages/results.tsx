7c18264ac13a35463f54ef00b26d235e16c7c2d1
Cleo Gao <celeo127@outlook.com>
2022-10-24 18:25:05 +0800

    finish with the backend

--- pages/results.tsx
+++ pages/results.tsx
@@ -1,37 +1,67 @@
 import Image from "next/image";
 import PrimaryLayout from "../components/layouts/primary/PrimaryLayout";
 import { NextPageWithLayout } from './page';
-import Search from "../components/utility/search/Search";
-import logo from '../assets/img/google-logo.png';
 import Link from "next/link";
 import { mockSearchResultProps } from "../components/utility/search-result/SearchResult.mocks";
 import  SearchResult  from '../components/utility/search-result/SearchResult';
+import { GetServerSideProps} from "next";
+import { ISearchData } from "../lib/search/types";
+import { IApiSearchResponseData } from "./api/search";
 
-const Results: NextPageWithLayout = () => {
 
+export interface IResults {
+  searchResults: ISearchData[];
+}
 
-  return (
-    <section className="flex flex-col items-center gap-y-5 ">
-        <div className={`flex flex-col space-y-8`}>
-            {[...new Array(6)].map((_, idx) => {
-                return <SearchResult key={idx} {...mockSearchResultProps.base} />;
-            })}
-        </div>
-      
-    
-    </section>
-  );
+export const getServerSideProps: GetServerSideProps<IResults> = async ({
+  query,
+}) => {
+  let searchResults: IApiSearchResponseData = [];
+  const searchTerm = query.search;
+
+  if(searchTerm && searchTerm.length > 0) {
+    const response = await fetch(`http://localhost:3000/api/search`, {
+      body: JSON.stringify({ searchTerm }),
+      headers : {
+        'Content-Type' : 'application/json',
+      },
+      method: 'POST',
+    });
+
+    searchResults = await response.json();
+  }
+  return {
+    props: {
+      searchResults,
+    },
+  };
 };
 
 
-export default Results;
 
-Results.getLayout = (page) => {
-  return (
-  <PrimaryLayout justify="items-start">
-    
-    {page}
-  
+  const Results: NextPageWithLayout<IResults> = ({ searchResults }) => {
+    const hasResults = searchResults.length > 0;
+    return (
+      <>
+      <section className="flex flex-col items-center gap-y-5">
+        {hasResults ? (
+          <div className={`flex flex-col space-y-8 `}>
+            {searchResults.map((result, idx) => {
+              return <SearchResult key={idx} {...result} />
+            })}
+          </div>
+        ) : (
+          <p>No results found.</p>
+        )}
+      </section>
+      </>
+    )
+  }
+
+  export default Results;
+
+  Results.getLayout = (page) => {
+    return <PrimaryLayout justify='items-start'>
+      {page}
     </PrimaryLayout>
-  );
-}
+  }
