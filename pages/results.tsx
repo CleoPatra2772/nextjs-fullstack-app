e95e92ff9db8ebb3bca2e178749c4aa447882728
Cleo Gao <celeo127@outlook.com>
2022-10-24 14:36:59 +0800

    finish front-end

---
+++ pages/results.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import PrimaryLayout from "../components/layouts/primary/PrimaryLayout";
+import { NextPageWithLayout } from './page';
+import Search from "../components/utility/search/Search";
+import logo from '../assets/img/google-logo.png';
+import Link from "next/link";
+import { mockSearchResultProps } from "../components/utility/search-result/SearchResult.mocks";
+import  SearchResult  from '../components/utility/search-result/SearchResult';
+
+const Results: NextPageWithLayout = () => {
+
+
+  return (
+    <section className="flex flex-col items-center gap-y-5 ">
+        <div className={`flex flex-col space-y-8`}>
+            {[...new Array(6)].map((_, idx) => {
+                return <SearchResult key={idx} {...mockSearchResultProps.base} />;
+            })}
+        </div>
+      
+    
+    </section>
+  );
+};
+
+
+export default Results;
+
+Results.getLayout = (page) => {
+  return (
+  <PrimaryLayout justify="items-start">
+    
+    {page}
+  
+    </PrimaryLayout>
+  );
+}
